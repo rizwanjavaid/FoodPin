1778ef1e6b0a673039828f746a8f5a5964487fe1
Rizwan Javaid <rizwan@rizwanjavaid.com>
2014-10-27 21:06:32 -0700

    First Commit

---
+++ FoodPin/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  FoodPin
+//
+//  Created by Rizwan Javaid on 10/26/14.
+//  Copyright (c) 2014 Rizwan Javaid. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
